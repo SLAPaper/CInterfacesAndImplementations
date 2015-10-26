1ccf222e4d0e9472c26551460191a1de27cc5c65
SLAPaper <slapaper.pku@gmail.com>
2015-10-26 13:28:33 +0800

    Completed List

--- List/List.c
+++ List/List.c
@@ -1,24 +1,109 @@
-#ifndef LIST_INCLUDED
-#define LIST_INCLUDED
+#include <stdarg.h>
+#include <stddef.h>
+#include "../Assert/Assert.h"
+#include "../Mem/Mem.h"
+#include "List.h"
 
 #define T List_T
-typedef struct T *T;
 
-struct T {
-    T rest;
-    void *first;
-};
+T List_push(T list, void *x) {
+    T p;
+    NEW(p);
+    p->first = x;
+    p->rest = list;
+    return p;
+}
 
-extern T List_append(T list, T tail);
-extern T List_copy(T list);
-extern T List_list(void *x, ...);
-extern T List_pop(T list, void **x);
-extern T List_push(T list, void *x);
-extern T List_reverse(T list);
-extern int List_length(T list);
-extern void List_free(T *list);
-extern void List_map(T list, void apply(void **x, void *cl), void *cl);
-extern void ** List_toArray(T list, void *end);
+T List_list(void *x, ...) {
+    T list, *p = &list;
 
+    va_list ap;
+    va_start(ap, x);
+    for (; x; x = va_arg(ap, void *)) {
+        NEW(*p);
+        (*p)->first = x;
+        p = &(*p)->rest;
+    }
+    *p = NULL;
+    va_end(ap);
+    return list;
+}
+
+T List_append(T list, T tail) {
+    T *p = &list;
+    while (*p)
+        p = &(*p)->rest;
+    *p = tail;
+    return list;
+}
+
+T List_copy(T list) {
+    T head, *p = &head;
+    for (; list; list = list->rest) {
+        NEW(*p);
+        (*p)->first = list->first;
+        p = &(*p)->rest;
+    }
+    *p = NULL;
+    return head;
+}
+
+T List_pop(T list, void **x) {
+    if (list) {
+        T head = list->rest;
+        if (x)
+            *x = list->first;
+        FREE(list);
+        return head;
+    }
+    else
+        return list;
+}
+
+T List_reverse(T list) {
+    T head = NULL, next;
+    for (; list; list = next) {
+        next = list->rest;
+        list->rest = head;
+        head = list;
+    }
+    return head;
+}
+
+int List_length(T list) {
+    int n;
+    for (n = 0; list; list = list->rest)
+        ++n;
+    return n;
+}
+
+void List_free(T *list) {
+    assert(list);
+
+    T next;
+    for (; *list; *list = next) {
+        next = (*list)->rest;
+        FREE(*list);
+    }
+}
+
+void List_map(T list, void apply(void **x, void *cl), void *cl) {
+    assert(apply);
+
+    for (; list; list = list->rest)
+        apply(&list->first, cl);
+}
+
+void ** List_toArray(T list, void *end) {
+    int n = List_length(list);
+    void ** arraylist = ALLOC((n + 1) * sizeof(*arraylist));
+
+    int i;
+    for (i = 0; i < n; ++i) {
+        arraylist[i] = list->first;
+        list = list->rest;
+    }
+    arraylist[i] = end;
+    return arraylist;
+}
 #undef T
-#endif
