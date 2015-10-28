e18a1fcb51715f39152048378af34c4a87f7c01d
SLAPaper <slapaper.pku@gmail.com>
2015-10-28 23:41:22 +0800

    Updated the properties of each project

--- CInterfacesAndImplementations/main.c
+++ CInterfacesAndImplementations/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../Arena/Arena.h"
+#include "../Arith/Arith.h"
+#include "../Assert/Assert.h"
+#include "../Atom/Atom.h"
+#include "../Except/Except.h"
+#include "../List/List.h"
+#include "../Mem/Mem.h"
+#include "../Stack/Stack.h"
+#include "../Table/Table.h"
 
 int main(int argc, char* argv[]) {
     printf("Hello World!\n");
