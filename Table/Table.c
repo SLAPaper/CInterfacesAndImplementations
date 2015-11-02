d461fe6b399719e956cf85512f2761df941a314c
SLAPaper <slapaper.pku@gmail.com>
2015-11-02 23:17:12 +0800

    finished Table

--- Table/Table.c
+++ Table/Table.c
@@ -64,3 +64,93 @@ void * Table_get(Table_T table, const void *key) {
     }
     return p ? p->value : NULL;
 }
+
+void * Table_put(Table_T table, const void *key, void *value) {
+    assert(table);
+    assert(key);
+
+    int i = table->hash(key) % table->size;
+    struct binding *p;
+    void *prev = NULL;
+    for (p = table->buckets[i]; p; p = p->link) {
+        if (table->cmp(key, p->key) == 0)
+            break;
+    }
+    if (p == NULL) {
+        NEW(p);
+        p->key = key;
+        p->link = table->buckets[i];
+        table->buckets[i] = p;
+        ++table->length;
+        prev = NULL;
+    }
+    else {
+        prev = p->value;
+    }
+    ++table->timestamp;
+    return prev;
+}
+
+int Table_length(Table_T table) {
+    assert(table);
+    return table->length;
+}
+
+void Table_map(Table_T table, void apply(const void *key, void **value, void *cl), void *cl) {
+    assert(table);
+    assert(apply);
+    unsigned int stamp = table->timestamp;
+    for (int i = 0; i < table->size; ++i) {
+        for (struct binding *p = table->buckets[i]; p; p = p->link) {
+            apply(p->key, p->value, cl);
+            assert(table->timestamp = stamp);
+        }
+    }
+}
+
+void * Table_remove(Table_T table, const void *key) {
+    assert(table);
+    assert(key);
+
+    ++table->timestamp;
+    int i = table->hash(key) % table->size;
+    for (struct binding **pp = &table->buckets[i]; *pp; pp = &(*pp)->link) {
+        if (table->cmp(key, (*pp)->key) == 0) {
+            struct binding *p = *pp;
+            void *value = p->value;
+            *pp = p->link;
+            FREE(p);
+            --table->length;
+            return value;
+        }
+    }
+    return NULL;
+}
+
+void ** Table_toArray(Table_T table, void *end) {
+    assert(table);
+
+    void **arraylist = ALLOC((2 * table->length + 1) * sizeof(*arraylist));
+    int j = 0;
+    for (int i = 0; i < table->size; ++i) {
+        for (struct binding *p = table->buckets[i]; p; p = p->link) {
+            arraylist[j++] = (void *)p->key;
+            arraylist[j++] = p->value;
+        }
+    }
+    arraylist[j] = end;
+    return arraylist;
+}
+
+void Table_free(Table_T *table) {
+    assert(table && *table);
+    if ((*table)->length > 0) {
+        for (int i = 0; i < (*table)->size; ++i) {
+            for (struct binding *p = (*table)->buckets[i], *q = NULL; p; p = q) {
+                q = p->link;
+                FREE(p);
+            }
+        }
+    }
+    FREE(*table);
+}
