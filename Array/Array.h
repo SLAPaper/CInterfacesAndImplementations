8ea4e73825d0dad8a054698eedde1af042098f4b
SLAPaper <slapaper.pku@gmail.com>
2015-11-08 23:07:40 +0800

    Finished Array.h and ArrayRep.h

--- Array/Array.h
+++ Array/Array.h
@@ -1,1 +1,13 @@
 #pragma once
+
+typedef struct Array_T *Array_T;
+
+//exported functions 117
+Array_T Array_new(int length, int size);
+void Array_free(Array_T *arraylist);
+int Array_length(Array_T arraylist);
+int Array_size(Array_T arraylist);
+void * Array_get(Array_T arraylist, int i);
+void * Array_put(Array_T arraylist, int i, void *elem);
+void Array_resize(Array_T arraylist, int length);
+Array_T Array_copy(Array_T arraylist, int length);
